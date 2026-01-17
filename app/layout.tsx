948a9d436e247bffcb43d4757378dbcb33496d7e
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-17 17:10:54 +0100

    feat: wrap UserProvider with WalletProvider in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import MiniAppProvider from "./components/MiniAppProvider";
 import { UserProvider } from "./providers/UserContext";
+import { WalletProvider } from "./providers/WalletProvider";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -45,11 +46,13 @@ export default function RootLayout({
       <body
         className={`${jetbrainsMono.variable} font-mono bg-black text-green-500 min-h-screen flex flex-col antialiased selection:bg-green-500 selection:text-black`}
       >
-        <UserProvider>
-          <MiniAppProvider>
-            {children}
-          </MiniAppProvider>
-        </UserProvider>
+        <WalletProvider>
+          <UserProvider>
+            <MiniAppProvider>
+              {children}
+            </MiniAppProvider>
+          </UserProvider>
+        </WalletProvider>
       </body>
     </html>
   );
