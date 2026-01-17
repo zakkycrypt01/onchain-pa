f16b27161852a9c4e60d313a1c7108d45c0fc166
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-17 17:58:22 +0100

    feat: add TypeScript definitions for w3m components

---
+++ app/components/w3m-components.d.ts
@@ -0,0 +1,20 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "w3m-connect-button": {
+      className?: string;
+      style?: React.CSSProperties;
+      children?: React.ReactNode;
+    };
+    "w3m-button": {
+      className?: string;
+      style?: React.CSSProperties;
+      children?: React.ReactNode;
+    };
+    "w3m-modal": {
+      className?: string;
+      style?: React.CSSProperties;
+      children?: React.ReactNode;
+    };
+  }
+}
+
