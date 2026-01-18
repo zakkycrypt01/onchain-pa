d49080ee80840c297b080b7787650540b861bf53
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:20:47 +0100

    test: add hook unit test placeholders

---
+++ app/__tests__/hooks.test.ts
@@ -0,0 +1,18 @@
+// RainbowKit hooks tests
+// Migration from Reown to RainbowKit
+
+describe("useRainbowKit", () => {
+  it("should return wallet address when connected", () => {
+    // TODO: Implement hook tests
+  });
+
+  it("should return undefined when disconnected", () => {
+    // TODO: Implement hook tests
+  });
+});
+
+describe("useRainbowKitModal", () => {
+  it("should return openModal function", () => {
+    // TODO: Implement modal tests
+  });
+});
