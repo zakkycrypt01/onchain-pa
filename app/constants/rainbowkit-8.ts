c88e111d0ec8732f4e73c3fbbaab55eb32d8c243
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:21:21 +0100

    chore(constants): add RainbowKit constants 8

---
+++ app/constants/rainbowkit-8.ts
@@ -0,0 +1,11 @@
+// RainbowKit Constant 8
+
+export const RAINBOWKIT_CONSTANT_8 = "rainbowkit_value_8";
+export const RAINBOWKIT_TIMEOUT_8 = 8000;
+export const RAINBOWKIT_RETRY_8 = 3;
+
+export default {
+  RAINBOWKIT_CONSTANT_8,
+  RAINBOWKIT_TIMEOUT_8,
+  RAINBOWKIT_RETRY_8,
+};
