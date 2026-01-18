c7a746ee7b5ff4f8ceda71228a0c126a2bc1c869
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:21:20 +0100

    feat(types): add RainbowKit type extension 4

---
+++ app/types/rainbowkit-ext-4.ts
@@ -0,0 +1,11 @@
+// RainbowKit Type Extension 4
+
+export interface RainbowKitExtension4 {
+  id: string;
+  name: string;
+  version: string;
+}
+
+export type RainbowKitExtensionConfig4 = RainbowKitExtension4 & {
+  enabled: boolean;
+};
