68bc9ef398444831c699d885d1afbb7513797ffd
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:21:21 +0100

    feat(types): add RainbowKit type extension 8

---
+++ app/types/rainbowkit-ext-8.ts
@@ -0,0 +1,11 @@
+// RainbowKit Type Extension 8
+
+export interface RainbowKitExtension8 {
+  id: string;
+  name: string;
+  version: string;
+}
+
+export type RainbowKitExtensionConfig8 = RainbowKitExtension8 & {
+  enabled: boolean;
+};
