eb176669c82c331682442609fb905bd1729c14ea
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:21:21 +0100

    feat(types): add RainbowKit type extension 10

---
+++ app/types/rainbowkit-ext-10.ts
@@ -0,0 +1,11 @@
+// RainbowKit Type Extension 10
+
+export interface RainbowKitExtension10 {
+  id: string;
+  name: string;
+  version: string;
+}
+
+export type RainbowKitExtensionConfig10 = RainbowKitExtension10 & {
+  enabled: boolean;
+};
