700c9129023f79c2f56e8643f178db7b30b8a2c0
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:20:08 +0100

    chore(providers): create provider index for clean imports

---
+++ app/providers/index.ts
@@ -0,0 +1,3 @@
+export { default as RainbowKitProvider } from "./RainbowKitProvider";
+export { UserProvider } from "./UserContext";
+export { default as MiniAppProvider } from "../components/MiniAppProvider";
