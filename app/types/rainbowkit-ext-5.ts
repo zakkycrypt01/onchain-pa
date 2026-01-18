adfcd56528075d9430aaaffe17149bc91e3e71e6
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:21:20 +0100

    feat(types): add RainbowKit type extension 5

---
+++ app/types/rainbowkit-ext-5.ts
@@ -0,0 +1,11 @@
+// RainbowKit Type Extension 5
+
+export interface RainbowKitExtension5 {
+  id: string;
+  name: string;
+  version: string;
+}
+
+export type RainbowKitExtensionConfig5 = RainbowKitExtension5 & {
+  enabled: boolean;
+};
