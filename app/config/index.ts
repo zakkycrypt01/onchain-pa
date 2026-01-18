331b3fc42a0496a01f23585909f7fb8b5dedb33c
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:20:43 +0100

    chore(config): create configuration index

---
+++ app/config/index.ts
@@ -0,0 +1,6 @@
+export { default as RAINBOWKIT_CONFIG } from "./rainbowkit";
+export { getWagmiConfig } from "./wagmi";
+export { default as THEME_CONFIG } from "./theme";
+export { SUPPORTED_CHAINS } from "./chains";
+export { default as CONNECTOR_CONFIG } from "./connectors";
+export { default as NETWORK_CONFIG } from "./networks";
