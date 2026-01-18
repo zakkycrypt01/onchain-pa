c1e30cf293ee8d1796a73cd781be0983e43f91c2
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:16:58 +0100

    feat(providers): create RainbowKitProvider skeleton component

---
+++ app/providers/RainbowKitProvider.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+// This file will contain the RainbowKit wallet provider setup
+// Migration from Reown/AppKit to RainbowKit
+
+export const RainbowKitProvider = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
