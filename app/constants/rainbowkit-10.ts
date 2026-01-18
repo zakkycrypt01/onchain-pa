949c9e2fd2fccafe378851539f4df92751efec19
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:21:21 +0100

    chore(constants): add RainbowKit constants 10

---
+++ app/constants/rainbowkit-10.ts
@@ -0,0 +1,11 @@
+// RainbowKit Constant 10
+
+export const RAINBOWKIT_CONSTANT_10 = "rainbowkit_value_10";
+export const RAINBOWKIT_TIMEOUT_10 = 10000;
+export const RAINBOWKIT_RETRY_10 = 2;
+
+export default {
+  RAINBOWKIT_CONSTANT_10,
+  RAINBOWKIT_TIMEOUT_10,
+  RAINBOWKIT_RETRY_10,
+};
