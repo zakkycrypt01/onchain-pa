7baa23d183af374424bff7b29990f49103dd1cb2
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-18 19:20:44 +0100

    test: add integration test template

---
+++ app/__tests__/integration.test.ts
@@ -0,0 +1,15 @@
+// RainbowKit integration tests
+
+describe("RainbowKit Integration", () => {
+  describe("Provider Setup", () => {
+    it("should initialize RainbowKit with correct config", () => {
+      // TODO: Add integration tests
+    });
+  });
+
+  describe("Wallet Connection", () => {
+    it("should handle wallet connection flow", () => {
+      // TODO: Add connection flow tests
+    });
+  });
+});
